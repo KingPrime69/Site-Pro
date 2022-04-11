81146255b4d56a12120508e85e06ceb04774579e
KingPrime69 <quentin.ripot@free.fr>
2022-04-11 18:20:08 +0200

    navbar + modal

---
+++ content/navbar.php
@@ -0,0 +1,10 @@
+<nav>
+  <div class="nav-wrapper black">
+    <a href="#!" class="brand-logo">Logo</a>
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="#modal1" class="material-icons modal-trigger">person_outline</a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -11,8 +11,22 @@
   </head>
 
   <body>
+    <?php require_once 'content/navbar.php'; ?>
+
     <h1>Gamebox</h1>
+
+    <div id="modal1" class="modal">
+      <div class="modal-content">
+        <h4>Modal Header</h4>
+        <p>A bunch of text</p>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+      </div>
+    </div>
     <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
   </body>
 </html>
