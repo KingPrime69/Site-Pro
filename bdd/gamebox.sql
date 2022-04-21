e0742cb4b1f6bfa83cc1515f7e0e5f3001b4a59e
KingPrime69 <quentin.ripot@free.fr>
2022-04-22 01:03:27 +0200

    bbd + messagerie

--- bdd/gamebox.sql
+++ bdd/gamebox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 21 avr. 2022 à 19:27
+-- Généré le : jeu. 21 avr. 2022 à 23:02
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `gamebox`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `object` varchar(50) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `object`, `text`) VALUES
+(1, 'test', 'test@test.fr', 'test', 'test test test test test test test test test '),
+(3, 'quentin', 'test@test.fr', 'Commande', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 -- --------------------------------------------------------
 
@@ -52,8 +76,8 @@ CREATE TABLE IF NOT EXISTS `css` (
 --
 
 INSERT INTO `css` (`id`, `activated`, `theme`, `background-image`, `below-image`, `Article-1`, `Article-2`, `Article-3`, `Article-4`, `Article-5`, `Article-6`, `color-font`, `style-font`, `link-font`, `title`) VALUES
-(1, 1, 'HALLOWEEN', 'img/fond.jpg', 'img/blood.png', 'img/lof.jpg', 'img/pkj.jpg', 'img/dbd.jpg', 'img/mystery.PNG', 'img/dbdfig.jpg', 'img/mystery.PNG', '#fff', 'Rubik Wet Paint, cursive', '<link href=\"https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&display=swap\" rel=\"stylesheet\">', 'LA GAMEBOX D\'OCTOBRE SUR LE THEME HALLOWEEN'),
-(2, 0, 'HIVER', '', '', '', '', '', '', '', '', 'white', 'Josefin Sans, sans-serif', '<link href=\"https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap\" rel=\"stylesheet\">', 'test'),
+(1, 1, 'HALLOWEEN', 'img/fond.jpg', 'img/blood.png', 'img/lof.jpg', 'img/pkj.jpg', 'img/dbd.jpg', 'img/mystery.PNG', 'img/dbdfig.jpg', 'img/mystery.PNG', '#780404', 'Rubik Wet Paint, cursive', '<link href=\"https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&display=swap\" rel=\"stylesheet\">', 'LA GAMEBOX D\'OCTOBRE SUR LE THEME HALLOWEEN'),
+(2, 0, 'HIVER', 'img/fond.jpg', '', 'img\\mystery.PNG', 'img\\mystery.PNG', 'img\\mystery.PNG', 'img\\mystery.PNG', 'img\\mystery.PNG', 'img\\mystery.PNG', '#00b8ff', 'Josefin Sans, sans-serif', '<link href=\"https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap\" rel=\"stylesheet\">', 'LA GAMEBOX DE DECEMBRE SUR LE THEME NOËL'),
 (3, 0, 'test', '', '', '', '', '', '', '', '', '#ecbb6e', 'My Soul, cursive', '<link href=\"https://fonts.googleapis.com/css2?family=My+Soul&display=swap\" rel=\"stylesheet\">', ''),
 (13, 0, 'qsdf', '', '', '', '', '', '', '', '', 'qsdf', '', 'test', ''),
 (12, 0, '', '', '', 'img/fond.jpg', 'img/fond.jpg', 'img/fond.jpg', 'img/fond.jpg', 'img/fond.jpg', '', '#069b34', '', '', ''),
@@ -72,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(40) NOT NULL,
   `password` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
@@ -82,7 +106,9 @@ INSERT INTO `user` (`id`, `activated`, `email`, `password`) VALUES
 (1, 1, 'test@test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
 (2, 1, 'qsdf@couille.com', '782dd27ea8e3b4f4095ffa38eeb4d20b59069077'),
 (3, 1, 'qsdfff@a', '10d58bd87102f18f6f8d2e7fc4600aab5ef5549c'),
-(4, 1, 'test@test.fr', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+(4, 0, 'test@test.fr', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
+(5, 1, 'quentin.ripot@free.fr', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
+(6, 0, '121@007', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- message.php
+++ message.php
@@ -7,23 +7,25 @@
     $pre->execute();
     $data = $pre->fetchAll(PDO::FETCH_ASSOC);
     ?>
-    <h2>Liste des utilisateur</h2>
+    <h2>Liste des Messages</h2>
     <?php
-    foreach ($data as $user):
+    foreach ($data as $message):
       ?>
       <div class="row">
-      <form class="" action="modifUser.php" method="post">
-          <p class="col"><?php echo $user['id']; ?> :</p>
-          <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-          <input type="email" name="email" value="<?php echo $user['email']; ?>" class="col l4">
-          <button class="btn waves-effect waves-light col l1 align" type="submit" name="action">répondre
+        <form class="" action="detailMessage.php" method="post">
+          <p class="col"><?php echo $message['id']; ?> :</p>
+          <input type="hidden" name="id" value="<?php echo $message['id']; ?>">
+          <h5 class="col l2">Mail: <?php echo $message['email']; ?></h5>
+          <h5 class="col l2">Objet: <?php echo $message['object']; ?></h5>
+          <button class="btn waves-effect waves-light col l1 align" type="submit" name="action">voir plus
             <i class="material-icons right">send</i>
           </button>
-      </form>
+          <button class="btn waves-effect waves-light col l1 align" type="submit" name="action">Traité
+            <i class="material-icons right">send</i>
+          </button>
+        </form>
       </div>
       <?php endforeach; ?>
-      <div class="center-align">
-      </div>
   <script type="text/javascript" src="js/jquery.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
