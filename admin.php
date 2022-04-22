d509532d500746151084c471617dbe4c1cc3c34c
Manuiva ATENI <mateni@gaming.tech>
2022-04-22 04:32:08 +0200

    Merge branch 'main' of https://github.com/KingPrime69/Site-Pro into main

--- admin.php
+++ admin.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-  <?php require_once 'helpers/head.php'; ?>
-    <?php require_once 'content/navbar.php'; ?>
-<body>
-  <div class="container center-align">
-    <div class="divider"></div>
-    <div class="section">
-      <a class="waves-effect waves-light btn" href="theme.php">Theme</a>
-    </div>
-    <div class="divider"></div>
-    <div class="section">
-      <a class="waves-effect waves-light btn" href="user.php">User</a>
-    </div>
-    <div class="divider"></div>
-    <div class="section">
-      <a class="waves-effect waves-light btn" href="message.php">Messagerie</a>
-    </div>
-  </div>
-  <!--JavaScript at end of body for optimized loading-->
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
-</body>
-=======
 <?php require_once 'helpers/config.php'; ?>
 <?php require_once 'helpers/head.php'; ?>
 <?php require_once 'content/navbar.php'; ?>
@@ -49,4 +25,3 @@
       <?php
     }
   ?>
->>>>>>> 5a92ba7c3df9020282f2b0155b8f266945277731
